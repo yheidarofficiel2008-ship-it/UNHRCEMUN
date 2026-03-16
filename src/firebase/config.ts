44d4e0a4d6d1ea20408b842eaa67330b3ca15d8b
Youssef Heidar <yousheidar@gmail.com>
2026-03-16 12:05:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2980531559-df4b0",
+  "appId": "1:844848531120:web:097adea7c15fd0c4755d75",
+  "apiKey": "AIzaSyAiadSKZF2c9-7zEeuJtiMkOcEkD4cWA0Y",
+  "authDomain": "studio-2980531559-df4b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844848531120"
+};
